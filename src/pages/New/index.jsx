0aa86f85a6ea3c72bcb18b4a840b473a740af1f5
Monica <contatomonicavaz@gmail.com>
2022-08-11 20:30:34 -0300

    fix rating on edit

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -64,7 +64,7 @@ export function New() {
       return null;
     }
 
-    if (rating < 0 || rating > 5 || rating.length === 0) {
+    if (rating < 0 || rating > 5) {
       setIsOpen(true);
       setAlertMessage('Digite uma nota de 0 a 5!');
 
